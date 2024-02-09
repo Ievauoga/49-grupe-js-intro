a679d2d9d95ae649cdb0187c6f3b69681966bca4
Ievauoga <157697527+Ievauoga@users.noreply.github.com>
2024-02-09 14:26:38 +0200

    index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+//Single-line comment
+//Single-line comment
+//Single-line comment
+
+/*
+
+Busimos temos:
+- duomenu tipai:
+    - skaiciai
+    - tekstas
+    - logines reiksmes
+    - sąrašas
+    - objektas
+- salygos (if)
+    - if
+    - ternary
+    - switch
+- ciklai (for)
+    -for
+    -while
+    -do-while
+    -for-of
+    -for-in
+- funkcijos
+   -function
+   -arrow function
+   -class
+- ivykiai (event listeners)
+
+*/
+
+console.log(123);
